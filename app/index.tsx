1315767e5a7c8667dca4d85fcffab6f2be24aac7
joana dias <joanadias@joanas-MacBook-Pro.local>
2025-06-29 00:37:26 +0100

    Test style updates

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,21 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#fff',
+  },
+});
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>I LOVE MIGUEL</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>I LOVE MIGUEL</Text>
     </View>
   );
 }
