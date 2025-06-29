96cf4059a2bf632bbf86bc74ec05787d20ee7579
joana dias <joanadias@joanas-MacBook-Pro.local>
2025-06-29 01:47:35 +0100

    Test gluestick-ui

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,11 @@
+import { Heading } from "@/components/ui/heading";
 import { Link } from 'expo-router';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home screen</Text>
+      <Heading size="3xl">Home screen</Heading>
       <Link href="/about" style={styles.button}>
         Go to About screen
       </Link>
@@ -18,9 +19,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#25292e',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  text: {
-    color: '#fff',
   },
   button: {
     fontSize: 20,
